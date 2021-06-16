2761ecc6e49b467ecac9fe60d55ec505077e2ba9
WarpedSinner <35859863+WarpedSinner@users.noreply.github.com>
2021-06-16 15:07:58 -0400

    Add project files.

---
+++ Snake Game/Snake Game.cpp
@@ -0,0 +1,219 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+struct Vec2 {
+    int x, y;
+};
+
+struct Data {
+    int Width = 1280;
+    int Height = 720;
+    const int RectSize = 20;
+    const size_t N = Width / RectSize;
+    const size_t M = Height / RectSize;
+    sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(Width, Height), "Snake Game");
+};
+
+
+class MyRects
+{
+public:
+    MyRects(Data _data) : data(_data){
+        InitRects();
+    }
+    ~MyRects() {
+        for (unsigned int x = 0; x < data.N; x++)
+            delete[] _rect[x];
+
+        delete[] _rect;
+    }
+
+    void DrawRect() {
+        for (int i = 0; i < data.N; i++) {
+            for (int j = 0; j < data.M; j++) {
+                data.window->draw(_rect[i][j]);
+            }
+        }
+    }
+    
+    void ChangeColor(int i,int j, sf::Color color) {
+        _rect[i][j].setFillColor(color);
+    }
+
+
+
+    sf::RectangleShape** _rect;
+private:
+    Data data;
+
+    void InitRects() {
+        _rect = new sf::RectangleShape* [data.N];
+        for (int x = 0; x < data.N; x++) {
+            _rect[x] = new sf::RectangleShape[data.N];
+        }
+        for (int i = 0; i < data.N; i++) {
+            for (int j = 0; j < data.M; j++) {
+                _rect[i][j] = sf::RectangleShape();
+                _rect[i][j].setSize(sf::Vector2f(data.RectSize, data.RectSize));
+                _rect[i][j].setPosition(sf::Vector2f(i * data.RectSize, j * data.RectSize));
+                _rect[i][j].setOutlineColor(sf::Color::Black);
+                _rect[i][j].setOutlineThickness(1);
+                if (i == data.N - 1 || i == 0 || j == data.M - 1 || j == 0) {
+                    _rect[i][j].setFillColor(sf::Color::Black);
+                }
+            }
+        }
+    }
+
+};
+
+class Player : private MyRects {
+public:
+    Player(Data _data) : MyRects(_data), data(_data){
+        InitPlayer();
+    }
+    ~Player() {
+
+    }
+
+    void InitPlayer() {
+        PlayerPos.x = data.N / 2;
+        PlayerPos.y = data.M / 2;
+
+        ChangeColor(PlayerPos.x, PlayerPos.y, sf::Color::Red);
+    }
+
+    void MovePlayer(int dir) {
+        CurrentDirection = dir;
+    }
+
+    void Update(){
+        if (PlayerPos.x == data.N || PlayerPos.x == 0 || PlayerPos.y == data.M || PlayerPos.y == 0)
+            return;
+        sf::Time elapsed = clock.getElapsedTime();
+
+        if (elapsed.asMilliseconds() < UpdateSpeed)
+            return;
+        else
+            clock.restart();
+        
+
+        OldPos = PlayerPos;
+        ChangeColor(PlayerPos.x, PlayerPos.y, sf::Color::White);
+        UpdateTail();
+        switch (CurrentDirection)
+        {
+        case Left:
+            PlayerPos.x--;
+            break;
+        case Right:
+            PlayerPos.x++;
+            break;
+        case Up:
+            PlayerPos.y--;
+            break;
+        case Down:
+            PlayerPos.y++;
+            break;
+        default:
+            break;
+        }
+        ChangeColor(PlayerPos.x, PlayerPos.y, sf::Color::Red);
+    }
+
+    enum Dir {
+        None = 0,
+        Left = 1,
+        Right = 2,
+        Up = 3,
+        Down = 4
+    };
+
+    void UpdateTail() {
+        if (tail.size() == 0) {
+            return;
+        }
+        ChangeColor(tail[TailLength - 1].x, tail[TailLength - 1].y, sf::Color::White);
+        std::cout << tail.size() << std::endl;
+        tail.pop_back();
+        tail.push_back(OldPos);
+        std::cout << tail.size() << std::endl;
+        for (size_t i = 0; i < tail.size(); i++)
+        {
+           ChangeColor(tail[i].x, tail[i].y, sf::Color::Red);
+        }
+        
+    }
+
+
+    void IncreaseSnakeSize() {
+        if (tail.size() <= TailLength) {
+            TailLength++;
+            tail.push_back(Vec2(OldPos));
+        }
+        
+    }
+
+
+    void Draw() { DrawRect(); }
+
+   
+
+private:
+    Data data;
+    Vec2 PlayerPos;
+    Vec2 OldPos;
+    int CurrentDirection;
+    int UpdateSpeed = 100;
+    sf::Clock clock;
+    std::vector<Vec2> tail;
+    int TailLength = 0;
+};
+
+
+int main()
+{
+    Data data;
+    Player p(data);
+
+    data.window->setFramerateLimit(60);
+
+    while (data.window->isOpen())
+    {
+        sf::Event event;
+        while (data.window->pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::Closed:
+                data.window->close();
+                break;
+            case sf::Event::KeyPressed:
+                if (event.key.code == sf::Keyboard::W)
+                    p.MovePlayer(p.Up);
+                if (event.key.code == sf::Keyboard::A)
+                    p.MovePlayer(p.Left);
+                if (event.key.code == sf::Keyboard::S)
+                    p.MovePlayer(p.Down);
+                if (event.key.code == sf::Keyboard::D)
+                    p.MovePlayer(p.Right);
+                if (event.key.code == sf::Keyboard::Space)
+                    p.IncreaseSnakeSize();
+                break;
+            default:
+                break;
+            }
+        }
+        
+        data.window->clear();
+        p.Draw();
+        data.window->display();
+        p.Update();
+    }
+
+   
+
+
+
+    return 0;
+}
